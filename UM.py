1c9689e14d238541ce9b10e113101b2b790054b1
Edwin_OwO <nicolaslabrune@gmail.com>
2022-10-20 09:55:27 -0300

    lol

--- UM.py
+++ UM.py
@@ -1,11 +1,11 @@
 import unittest
-from operacion import Operacion 
+from operacion import Operacion
 
 class MyTest(unittest.TestCase):
 
     def test_suma(self):
-        operacion = Operacion
-        self.assertEqual(operacion.suma (3,7),7)
+        operacion = Operacion()
+        self.assertEqual(operacion.suma(3,4),7)
 
 if __name__== "__main__":
     unittest.main()
